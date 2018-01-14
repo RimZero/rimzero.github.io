98a5495f63d9cb2076c17124c161677bc70bf5ac
Hang CHEN <hang22c@gmail.com>
2018-01-14 04:40:27 -0500

    ADD new playtype style

--- playtype.py
+++ playtype.py
@@ -7,11 +7,14 @@ import os
 CATEGORIES = ['Transition', 'Isolation', 'PRBallHandler', 'PRRollman',
               'Postup', 'Spotup', 'Handoff', 'Cut', 'OffScreen',
               'OffRebound', 'Misc']
-
-VALUE_KEYS = [u'P', u'GP', u'Poss', u'Time', u'Points',
-              u'FGM', u'FGA', u'PPP', u'WorsePPP', u'BetterPPP',
-              u'FG', u'aFG', u'FT', u'TO', u'SF', u'PlusOne', u'Score', ]
-PRIMARY_KEYS = ['Playtype', 'Team', 'Player']
+BY_TYPES = ['team', 'player']
+PLAYER_VALUE_KEYS = [u'P', u'GP', u'Poss', u'Time', u'Points',
+                     u'FGM', u'FGA', u'PPP', u'WorsePPP', u'BetterPPP',
+                     u'FG', u'aFG', u'FT', u'TO', u'SF', u'PlusOne', u'Score', ]
+VALUE_KEYS = {'team': PLAYER_VALUE_KEYS[1:], 'player': PLAYER_VALUE_KEYS}
+PLAYER_PRIMARY_KEYS = ['Playtype', 'Team', 'Player']
+PRIMARY_KEYS = {
+    'team': PLAYER_PRIMARY_KEYS[:-1], 'player': PLAYER_PRIMARY_KEYS}
 
 TYPES = ['offensive', 'defensive']
 
@@ -26,26 +29,33 @@ HEADERS = {
 }
 
 
-def api(category, type):
-    return "http://stats-prod.nba.com/wp-json/statscms/v1/synergy/player/?season=2017&category=#{category}&limit=500&names=#{type}&seasonType=Reg".replace(
-        '#{category}', category).replace('#{type}', type)
+def api(category, type, by_type):
+    return "http://stats-prod.nba.com/wp-json/statscms/v1/synergy/#{by_type}/?season=2017&category=#{category}&limit=500&names=#{type}&seasonType=Reg".replace(
+        '#{category}', category).replace('#{type}', type).replace('#{by_type}', by_type)
 
 
 if __name__ == '__main__':
-    for type in TYPES:
-        players = []
-        print(type)
-        for category in CATEGORIES:
-            print(category)
-            res = get(api(category, type), headers=HEADERS, timeout=50)
-            res.raise_for_status()
-            for row in res.json()['results']:
-                player = {'Player': ', '.join(
-                    [row['PlayerFirstName'], row['PlayerLastName']]),
-                    'Team': row['TeamName'], 'Playtype': category}
-                for key in VALUE_KEYS:
-                    player[key] = row[key]
-                players.append(player)
-        print(len(players))
-        df = pd.DataFrame(players)[PRIMARY_KEYS + VALUE_KEYS]
-        df.to_csv(os.path.join('data', 'playtype_' + type + '.csv'), index=False)
+    for by_type in BY_TYPES:
+        for type in TYPES:
+            items = []
+            print(type)
+            for category in CATEGORIES:
+                print(category)
+                res = get(api(category, type, by_type),
+                          headers=HEADERS, timeout=50)
+                res.raise_for_status()
+                for row in res.json()['results']:
+                    if 'PlayerFirstName' in row:
+                        item = {'Player': ', '.join(
+                            [row['PlayerFirstName'], row['PlayerLastName']]),
+                            'Team': row['TeamName'], 'Playtype': category}
+                    else:
+                        item = {'Team': row['TeamName'], 'Playtype': category}
+                    for key in VALUE_KEYS[by_type]:
+                        item[key] = row[key]
+                    items.append(item)
+            print(len(items))
+            df = pd.DataFrame(
+                items)[PRIMARY_KEYS[by_type] + VALUE_KEYS[by_type]]
+            df.to_csv(os.path.join('data', by_type + '_playtype_' +
+                                   type + '.csv'), index=False)
